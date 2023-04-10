5b2e49bcb0fffda06de6c32b484f05ee79c96a83
tutor-1v1 <liumeng64@gmail.com>
2023-04-10 11:32:28 +0800

    WeChat cstutorcs COMP9024 23T1 MyPageRank

---
+++ DLListStr.h
@@ -0,0 +1,40 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+// DLListStr.h - Interface to doubly-linked list ADT
+// Written by John Shepherd, March 2013
+// Last modified, Ashesh Mahidadia, Jan 2020 
+
+#ifndef DLListStr_H
+#define DLListStr_H
+
+#include <stdio.h>
+#include "DLListStr.h"
+
+// External view of DLListStr
+// Implementation given in DLListStr.c
+// Implements a DLListStr of string 
+
+typedef struct DLListRep *DLListStr;
+
+
+
+
+// create a new empty DLListStr
+DLListStr newDLListStr();
+
+
+
+
+
+
+// free up all space associated with list
+void freeDLListStr(DLListStr);
+
+// display items from a DLListStr, comma separated
+void showDLListStr(DLListStr L);
+
+
+
+#endif
